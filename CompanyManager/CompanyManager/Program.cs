285838e913a3d6ae7402f0871923666902a4e681
Glenn Cuevas <glenn.cuevas+github@gmail.com>
2020-08-04 14:05:08 -0700

    CompanyManager initial checkin with project references to Core

---
+++ CompanyManager/CompanyManager/Program.cs
@@ -0,0 +1,19 @@
+﻿using Core.DAL.Repos;
+using Core.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyManager
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Company company = Companies.GetCompany(2);
+			Console.WriteLine($"Loaded company ID: { company.Id } Name: { company.Name}");
+		}
+	}
+}
